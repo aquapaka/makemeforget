d9cb3e8b5fd959fef074a24fa9ae298764056398
aquapaka <aqua.tamlong@gmail.com>
2024-01-11 23:52:01 +0700

    Add favicon, meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 
 <svelte:head>
 	<title>Make me forget</title>
+	<meta name="description" content="You want to forget a person? This page can help you forget. Try to forget now.">
 </svelte:head>
 
 <slot />
